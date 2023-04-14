ad004268ce25acf337e77c6f0dbb7d62eb3cb138
Sapitorico <renzoyaques@gmail.com>
2023-04-14 16:45:07 -0300

    handler for 404 errors that returns a JSON-formatted 404

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """application"""
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -20,6 +20,11 @@ port = int(getenv('HBNB_API_PORT', 500))
 def close_storage(exception):
     """Close the storage"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == '__main__':
